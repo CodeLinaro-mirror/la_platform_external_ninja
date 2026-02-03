2d46428cb032310ce686e5628f0aec6f996b2d62
Colin Cross <ccross@android.com>
2026-02-03 10:52:07 -0800

    ninja: use toolchain/ndk_kokoro/kokoro_build.sh
    Use the shared kokoro_build.sh to reduce duplication in the cmake,
    ninja and python kokoro builds.
    
    Bug: 474214382
    Test: KOKORO_HOST_ROOT_DIR=$PWD KOKORO_ROOT_DIR=/tmp/build docker run -eKOKORO_HOST_ROOT_DIR -eKOKORO_ROOT_DIR -v/var/run/docker.sock:/var/run/docker.sock -v$PWD:/tmp/build --entrypoint /tmp/build/external/ninja/kokoro/kokoro_build.sh ndk-docker
    Test: external/ninja/kokoro/kokoro_build.sh
    Change-Id: I9913d2a06e985d509f3ab841fcf1dad6aca5d98a

--- kokoro/kokoro_build.sh
+++ kokoro/kokoro_build.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
 set -e
+set -x
 
-top=$(cd $(dirname $0)/../../.. && pwd)
-ninja_src=$top/external/ninja
-
-# On Linux, enter the Docker container and reinvoke this script.
-if [ "$(uname)" == "Linux" -a "$SKIP_DOCKER" == "" ]; then
-    docker build -t ndk-ninja $ninja_src/kokoro
-    export SKIP_DOCKER=1
-    docker run -v$top:$top -eKOKORO_BUILD_ID -eSKIP_DOCKER \
-      --entrypoint $ninja_src/kokoro/kokoro_build.sh \
-      ndk-ninja
-    exit $?
-fi
-
-$ninja_src/kokoro/build.py
+kokoro_dir=$(cd $(dirname $0) && pwd)
+top=$(cd $kokoro_dir/../../.. && pwd)
+$top/toolchain/ndk-kokoro/kokoro_build.sh $kokoro_dir $kokoro_dir/build.py
